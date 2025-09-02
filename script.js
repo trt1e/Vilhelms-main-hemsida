ee32501a5b50a9b57bf99b017149f5f73701a279
trtle <77798301+trt1e@users.noreply.github.com>
2025-09-02 18:12:03 +0200

    Rebuild the entire webbsite from scratch
    now it can scale better and be better

---
+++ script.js
@@ -0,0 +1,45 @@
+Östra_Löken_utgåvor = [
+    "./östra_löken_pdf/Östra Löken upplaga 1.pdf",
+    "./östra_löken_pdf/Östra Löken upplaga 2.pdf",
+    "./östra_löken_pdf/Östra Löken upplaga 3.pdf",
+    "./östra_löken_pdf/Östra Löken upplaga 4.pdf",
+    "./östra_löken_pdf/Östra Löken upplaga 5.pdf",
+    "./östra_löken_pdf/Östra Löken upplaga 6.pdf",
+    "./östra_löken_pdf/Östra Löken upplaga 7.pdf"
+]; // sätter upp så vi kan pulla destinationerna på PDF:erna
+Östra_Löken_utgåvor_namn = [
+    "Östra Löken upplaga 1",
+    "Östra Löken upplaga 2",
+    "Östra Löken upplaga 3",
+    "Östra Löken upplaga 4",
+    "Östra Löken upplaga 5",
+    "Östra Löken upplaga 6",
+    "Östra Löken upplaga 7"
+]; // Här sätter vi upp namnen så att de kan sättas under PDF:en
+let mängdÖstraLöken;
+mängdÖstraLöken = Östra_Löken_utgåvor.length;
+let Östra_Löken_Ticker;
+Östra_Löken_Ticker = 0; 
+
+/*alert("test")*/
+
+function Östra_Löken_Right_Button_Down() {
+    Östra_Löken_Ticker = Östra_Löken_Ticker + 1; // öka countern
+
+    if (Östra_Löken_Ticker > (mängdÖstraLöken - 1)) { // vi ser till att om vi är på högsta tickern (högsta upplagan av Löken) så går vi till första
+        Östra_Löken_Ticker = 0;
+    }
+
+    document.getElementById("Östra_Löken_Titel_Inehål").innerHTML = Östra_Löken_utgåvor_namn[Östra_Löken_Ticker]; //uppdatera titeln
+    document.getElementById("Östra_Löken_Artikel_PDF").src = Östra_Löken_utgåvor[Östra_Löken_Ticker];
+};
+function Östra_Löken_Left_Button_Down() { //här gör vi samma sak bara åt andra hållet
+    Östra_Löken_Ticker = Östra_Löken_Ticker - 1;
+
+    if (Östra_Löken_Ticker < 0) { // vi ser till att om vi går under 0 återvänder vi till högsta
+        Östra_Löken_Ticker = (mängdÖstraLöken - 1);
+    }
+
+    document.getElementById("Östra_Löken_Titel_Inehål").innerHTML = Östra_Löken_utgåvor_namn[Östra_Löken_Ticker]; 
+    document.getElementById("Östra_Löken_Artikel_PDF").src = Östra_Löken_utgåvor[Östra_Löken_Ticker];
+};
