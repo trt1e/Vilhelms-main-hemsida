409119766f32f38e3228aabf28502476abd443ca
trt1e <vilhelm.grill@gmail.com>
2026-03-31 16:52:51 +0200

    made text clearer on webbpages page

--- webbsidor/js/hover_effect.js
+++ webbsidor/js/hover_effect.js
@@ -1,6 +1,7 @@
 const backgroundElements = document.getElementsByClassName("main_content_card glass_background");
 const foregroundElements = document.getElementsByClassName("main_content_card glass_foreground");
 const cardImgElements = document.getElementsByClassName("main_content_card_img");
+const cardTextElements = document.getElementsByClassName("main_card_text");
 
 addEventListener("mouseover", (event) => {
     for (let elementNmr = 0; elementNmr < backgroundElements.length; elementNmr++) {
@@ -8,10 +9,12 @@ addEventListener("mouseover", (event) => {
             backgroundElements[elementNmr].classList.add("backgroundcard_hover_active");
             foregroundElements[elementNmr].classList.add("foregroundcard_hover_active");
             cardImgElements[elementNmr].classList.add("main_content_card_img_hover_active");
+            cardTextElements[elementNmr].classList.add("main_card_text_hover");
         } else {
             backgroundElements[elementNmr].classList.remove("backgroundcard_hover_active");
             foregroundElements[elementNmr].classList.remove("foregroundcard_hover_active"); 
             cardImgElements[elementNmr].classList.remove("main_content_card_img_hover_active");
+            cardTextElements[elementNmr].classList.remove("main_card_text_hover");
         };
     };
 });
